1ee6ea3b9b56c001c462ba3d2d87c620e55b2405
shadrickkipamba11 <shadrickkipamba11@gmai.com>
2025-08-17 06:04:15 +0200

    zz

--- vite.config.js
+++ vite.config.js
@@ -60,7 +60,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: process.env.VITE_API_BASE_URL || 'https://app.kipdesky.com',
+        target: 'https://app.kipdesky.com',
         changeOrigin: true,
         secure: false
       }
